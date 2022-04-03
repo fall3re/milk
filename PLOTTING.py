bb1bbe50b24b0e45723efac70b96e280fab6709d
Yunzhe Li <fall3re@gmail.com>
2022-04-03 15:23:03 +0100

    PLOTTING trimmed unnecessary functionality.

--- PLOTTING.py
+++ PLOTTING.py
@@ -10,24 +10,19 @@ def add_to_plot(ax, pos, vel):
         in_cell = (cell * D_Z < pos[2]) & (pos[2] < (cell + 1) * D_Z)
         profile[cell] = np.mean(vel[1][in_cell])
 
-def plot_against_time(parameter):
+def plot_against_time(parameter, y1=None, y2=None):
     fig = plt.figure(figsize=(6, 4), dpi=80)
     plt.plot(parameter, label='Caption - Edit', color='black')
+    if y1 is not None and y2 is not None:
+        plt.ylim([y1, y2])
     plt.show()
 
 def plot_results(y_velocity):
     fig = plt.figure(figsize=(6, 4), dpi=80)
-    ax2 = plt.gca()
-    tt = D_T * np.linspace(1, NUM_TIMESTEPS, num=NUM_TIMESTEPS) / TIME
-    bgk = np.zeros(tt.shape)
-    for index in range(NUM_TIMESTEPS):
-        xx = np.linspace(tt[index] / 10000, tt[index], num=10000)
-        bgk[index] = 0.5 * (1 + np.trapz(np.exp(-xx) / xx * special.iv(1, xx), x=xx))
-    plt.plot(tt * 2.5, bgk, label='BGK theory', color='red')
-    plt.plot(tt, np.mean(y_velocity, axis=0).reshape((NUM_TIMESTEPS, 1)) / WALL_VELOCITY, label='DSMC', color='black')
-    plt.xlabel(r'$t/\tau$')
-    plt.ylabel(r'$u_y(z=0)/u_w$')
-    ax2.set(xlim=(0, NUM_MEAN_FREE_TIMES), ylim=(0.5, 1.1))
-    ax2.legend(loc='upper left')
+    ax = plt.gca()
+    plt.xlabel(r'$Timestep \tau$')
+    plt.ylabel(r'$Velocity v_y(z=0)$')
+    ax.set(xlim=(0, NUM_MEAN_FREE_TIMES), ylim=(0.5, 1.1))
+    ax.legend(loc='upper left')
     plt.savefig('dsmc.png', dpi=240)
     plt.show()
