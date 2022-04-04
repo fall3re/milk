113b315cda3fbe4c6cc43ca8b2a4ef86c3c4f462
Yunzhe Li <fall3re@gmail.com>
2022-04-04 16:10:48 +0100

    Final version of plotting.py

--- PLOTTING.py
+++ PLOTTING.py
@@ -1,7 +1,7 @@
-from CONFIG import D_Z,NUM_CELLS, NUM_TIMESTEPS, WALL_VELOCITY, D_T, TIME, NUM_MEAN_FREE_TIMES
+from CONFIG import D_Z,NUM_CELLS
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy import special
+
 
 
 def add_to_plot(ax, pos, vel):
@@ -11,18 +11,17 @@ def add_to_plot(ax, pos, vel):
         profile[cell] = np.mean(vel[1][in_cell])
 
 def plot_against_time(parameter, y1=None, y2=None):
-    fig = plt.figure(figsize=(6, 4), dpi=80)
+    fig = plt.figure()
     plt.plot(parameter, label='Caption - Edit', color='black')
     if y1 is not None and y2 is not None:
         plt.ylim([y1, y2])
     plt.show()
 
-def plot_results(y_velocity):
-    fig = plt.figure(figsize=(6, 4), dpi=80)
-    ax = plt.gca()
+def plot_results(y_velocity, y1=None, y2=None):
+    figure = plt.plot(y_velocity)
     plt.xlabel(r'$Timestep \tau$')
     plt.ylabel(r'$Velocity v_y(z=0)$')
-    ax.set(xlim=(0, NUM_MEAN_FREE_TIMES), ylim=(0.5, 1.1))
-    ax.legend(loc='upper left')
-    plt.savefig('dsmc.png', dpi=240)
+    if y1 is not None and y2 is not None:
+        plt.ylim([y1, y2])
+    plt.savefig('aaa.png', dpi=240)
     plt.show()
